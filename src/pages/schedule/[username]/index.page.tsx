3e9427ffdd84cae549384b0693a4dc17ca300ea5
Leonardo Bonfim <leo.bonfim.95@gmail.com>
2023-01-23 23:31:55 -0300

    feat: Start calendar page

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,67 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '../../../lib/prisma'
+import { ScheduleForm } from './ScheduleForm/index.page'
+import { Container, UserHeader } from './styles'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({
+  user: { avatarUrl, bio, name },
+}: ScheduleProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={avatarUrl} />
+        <Heading>{name}</Heading>
+        <Text>{bio}</Text>
+      </UserHeader>
+
+      <ScheduleForm />
+    </Container>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  const { name, bio, avatar_url: avatarUrl } = user
+
+  console.log(avatarUrl)
+
+  return {
+    props: {
+      user: {
+        name,
+        bio,
+        avatarUrl,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  }
+}
--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,9 +1,18 @@
-export function getWeekDays(): string[] {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+export function getWeekDays({
+  short = false,
+}: GetWeekDaysParams = {}): string[] {
   const formatter = new Intl.DateTimeFormat('pat-BR', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
-    .map((weekDay) =>
-      weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1)),
-    )
+    .map((weekDay) => {
+      if (short) {
+        return weekDay.substring(0, 3).toUpperCase()
+      }
+
+      return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
+    })
 }

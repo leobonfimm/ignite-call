07c9932cad06b039fe8270897f7b42f6458df377
Leonardo Bonfim <leo.bonfim.95@gmail.com>
2023-02-22 21:21:17 -0300

    feat: Deploy project

--- src/pages/schedule/[username]/ScheduleForm/index.page.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.page.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { CalendarStep } from './CalendarStep'
 import { ConfirmStep } from './ConfirmStep'
 
-export function ScheduleForm() {
+export default function ScheduleForm() {
   const [selectedDateTime, setSelectedDateTime] = useState<Date | null>()
 
   function handleClearSelectedDateTime() {
